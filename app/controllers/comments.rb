9dfbff025c666f0cd7c4394f0b89be7ceb65b2f0
Don <dholness2@gmail.com>
2015-05-24 20:14:06 -0400

    updates_to_delete_features_and_time

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,9 +1,23 @@
-get '/comments/new' do
-  erb :'comments/new'
-end
+
+
+
+
+
+
+>>>>>>> updates_to_delete_features_and_time
 
 post '/comments' do
   new_comment = Comment.new(params[:comment])
   return [500, "Invaild User"] unless new_comment.save
   redirect "/users/#{new_comment.author_id}"
+end
+
+
+delete '/comments/:user_id/:comment_id/delete' do
+  if session[:user_id] == params[:user_id]
+    comment = Comment.find_by(id: params[:comment_id] )
+    user_id = params[:user_id]
+    comment.destroy
+    redirect "/users/#{user_id}"
+  end
 end
